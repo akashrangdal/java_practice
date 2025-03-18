6da83fc63225e99612a14eb7ba2ffaad5ef0a0a7
akashrangdal <132357609+akashrangdal@users.noreply.github.com>
2025-03-18 19:41:27 +0530

    Update Main.java

--- SpringProject2/src/in/sp/main/Main.java
+++ SpringProject2/src/in/sp/main/Main.java
@@ -13,7 +13,7 @@ public class Main {
 		Student std = (Student) context.getBean("stdId");
 		std.display();
 <<<<<<< HEAD
-		System.out.println("Hi");
+		System.out.println("Hikjsdhkfkskj");
 =======
 		askjfslkj
 >>>>>>> bd2cec8628107897f3856b62ceb6efa010c215bd
